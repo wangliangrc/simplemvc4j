c405f62939e41a95cc03d16784b0690fd0f26de4
wdsgyj@gmail.com <wdsgyj@gmail.com@b65902d3-a1a6-4862-3571-31088cb2e1b5>
2011-12-01 04:47:35 +0000

    Removed file/folder

--- ApplicationFramework/src/com/clark/android/annotation/ViewListener.java
+++
@@ -1,9 +0,0 @@
-package com.clark.android.annotation;
-
-public enum ViewListener {
-    ON_CLICK, ON_FOCUS_CHANGE, ON_KEY, ON_LONG_CLICK, ON_TOUCH,
-
-    ON_ITEM_CLICK, ON_ITEM_LONG_CLICK, ON_ITEM_SELECTED,
-
-    ON_SCROLL, RECYCLER
-}
